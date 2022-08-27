b57494f75695f14d3e076246a468edf5d28ff57d
abdelhak <abdelhak@groupado.com>
2022-08-27 16:52:07 +0100

    refactor: remove unused router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,12 +2,10 @@ import superjson from "superjson";
 
 import { createRouter } from "utils/context";
 import { usersRouter } from "./users";
-import { authRouter } from "./auth";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("users.", usersRouter)
-  .merge("auth.", authRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
