5a996e655cb522486ac97b6a79bac107e4779eb8
abdelhak <abdelhak@groupado.com>
2022-09-05 14:04:17 +0100

    feat: add infer-types inferences to trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -7,4 +7,9 @@ import { createContext } from "utils/context";
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
+  onError({ error }) {
+    if (error.code === "INTERNAL_SERVER_ERROR") {
+      console.error("Something went wrong", error);
+    }
+  },
 });
