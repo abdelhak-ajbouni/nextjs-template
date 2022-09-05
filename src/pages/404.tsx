37471f52557b646c7ff898e4337ad55883374be0
abdelhak <abdelhak@groupado.com>
2022-09-05 14:03:28 +0100

    refactor: update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link'
 import { RiFileWarningLine } from 'react-icons/ri'
 import { WiDirectionLeft } from 'react-icons/wi'
 
+import Light from 'layouts/Light'
 import Container from "components/common/Container";
+import Box from 'components/common/Box'
 
 const NotFound: NextPage = () => {
   return (
@@ -12,20 +14,21 @@ const NotFound: NextPage = () => {
       <Head>
         <title>Page Not Found</title>
         <meta name="description" content="page not found" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="not-found">
         <Container className="flex justify-center">
-          <div className="bg-primary py-16 px-20 my-20 rounded-lg text-center">
+          <Box className="bg-primary py-16 px-20 rounded-lg text-center">
             <RiFileWarningLine size={120} className="text-primary m-auto" />
             <p className="text-primary text-xl m-4">Sorry , The page you are looking for cannot be found</p>
             <Link href='/'><span className="text-blue-600 cursor-pointer flex justify-center items-center"><WiDirectionLeft />Go to home page</span></Link>
-          </div>
+          </Box>
         </Container>
       </div>
     </>
   );
 };
+
+NotFound.Layout = Light
 
 export default NotFound;
